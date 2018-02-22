b47b94ff81ab467147040e334e6777da436add70
nvidya.karthik <nvidya.karthik@gmail.com>
2018-02-22 12:34:09 -0700

    added font family and size down image

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -26,57 +26,58 @@ $(document).ready(function () {
     $("#line2").html("");
     $('#defender').append($('#enemy >.selected'));
   });
-  $("#line2").on('click','#restart', function () {
-      location.reload();
+  $("#line2").on('click', '#restart', function () {
+    location.reload();
     //console.log("inside")
     //alert("test");
 
   });
 
   $("#attack").click(function () {
-    if(!$.trim( $('#defender').html() ).length){
-       $("#line1").html("No enemy here pick a new one.")
+    if (!$.trim($('#defender').html()).length) {
+      $("#line1").html("No enemy here pick a new one.")
 
     }
+    else {
+      if (objName[opponent]["healthPoints"] >= 0 && objName[player]["healthPoints"] >= 0) {
 
-    if (objName[opponent]["healthPoints"] >= 0 && objName[player]["healthPoints"] >= 0) {
-
-      //player attacking opponent
-      var opponentDamage = objName[player]["attackPower"] * (upPower++);
-      $("#line1").html("you attacked " + opponent + " for " + opponentDamage + " damage.");
-      //reducing opponents healthpoints
-      objName[opponent]["healthPoints"] = objName[opponent]["healthPoints"] - opponentDamage;
-      if (objName[opponent]["healthPoints"] < 0) {
-        console.log("test"+ !$.trim( $('#enemy').html() ).length );
-        if(!$.trim( $('#enemy').html() ).length ) {
-          $("#"+opponent).remove();
-          $("#line1").html("You win!!!");
-          $("#line2").html("");
-          $("#line2").append("<button id='restart' type='submit' value='restart'>Restart</button>");
+        //player attacking opponent
+        var opponentDamage = objName[player]["attackPower"] * (upPower++);
+        $("#line1").html("you attacked " + opponent + " for " + opponentDamage + " damage.");
+        //reducing opponents healthpoints
+        objName[opponent]["healthPoints"] = objName[opponent]["healthPoints"] - opponentDamage;
+        if (objName[opponent]["healthPoints"] < 0) {
+          console.log("test" + !$.trim($('#enemy').html()).length);
+          if (!$.trim($('#enemy').html()).length) {
+            $("#" + opponent).remove();
+            $("#line1").html("You win!!!");
+            $("#line2").html("");
+            $("#line2").append("<button id='restart' type='submit' value='restart'>Restart</button>");
+          }
+          else {
+            $("#" + opponent).remove();
+            $("#line1").html("You have defeated the " + opponent + " you can choose to fight another enemy.");
+            $("#line2").html("");
+          }
         }
-        else{
-          $("#"+opponent).remove();
-          $("#line1").html("You have defeated the "+opponent+" you can choose to fight another enemy.");
-          $("#line2").html("");
-        }       
-      }
-      else{
-      $("#" + opponent + " .hPoint").html(objName[opponent]["healthPoints"]);
-      //opponent attacks player now
-      //line printed in user screen
-      $("#line2").html(opponent + " attacked  you back for " + objName[opponent]["counterAttackPower"] + " damage.");
-      //reducing player healthpoints and storing it in object
-      objName[player]["healthPoints"] = objName[player]["healthPoints"] - objName[opponent]["counterAttackPower"];
-      $("#" + player + " .hPoint").html(objName[player]["healthPoints"]);
+        else {
+          $("#" + opponent + " .hPoint").html(objName[opponent]["healthPoints"]);
+          //opponent attacks player now
+          //line printed in user screen
+          $("#line2").html(opponent + " attacked  you back for " + objName[opponent]["counterAttackPower"] + " damage.");
+          //reducing player healthpoints and storing it in object
+          objName[player]["healthPoints"] = objName[player]["healthPoints"] - objName[opponent]["counterAttackPower"];
+          $("#" + player + " .hPoint").html(objName[player]["healthPoints"]);
 
-      if (objName[player]["healthPoints"] < 0) {
-        //updating in User 
-        $("#line1").html("You have been defeated GAME OVER!!!");
-        $("#line2").html("");
-        $("#line2").append("<button id='restart' type='submit' value='restart'>Restart</button>");
+          if (objName[player]["healthPoints"] < 0) {
+            //updating in User 
+            $("#line1").html("You have been defeated GAME OVER!!!");
+            $("#line2").html("");
+            $("#line2").append("<button id='restart' type='submit' value='restart'>Restart</button>");
+          }
+        }
+
       }
-    }
-      
     }
   });
 
