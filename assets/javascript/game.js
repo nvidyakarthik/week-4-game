914c59768473a8526572f3651a4b0bb7a64145f5
nvidya.karthik <nvidya.karthik@gmail.com>
2018-02-21 22:26:53 -0700

    added code to attack button onclick event

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,78 @@
- $( function() {
-    $( "#harry" ).draggable();
-  } );
-  $( function() {
-    $( "#draggable2" ).draggable();
-  } );
-  $( function() {
-    $( "#draggable3" ).draggable();
-  } );
-  $( function() {
-    $( "#draggable4" ).draggable();
-  } );
-  $('#characterbox').on('click','.character',function() {
-      $(this).removeClass('character');
-      $(this).addClass('selected');      
-      $('#enemy').append( $('#characterbox >.character') );
-}); 
-$('#enemy').on('click','.character',function() {
-    $(this).removeClass('character');
-    $(this).addClass('selected');      
-    $('#defender').append( $('#enemy >.selected') );
-}); 
+$(document).ready(function () {
+  var objName = {
+    harry: { healthPoints: 120, attackPower: 8, counterAttackPower: 8 },
+    hermione: { healthPoints: 100, attackPower: 5, counterAttackPower: 5 },
+    snape: { healthPoints: 150, attackPower: 6, counterAttackPower: 20 },
+    malfoy: { healthPoints: 180, attackPower: 7, counterAttackPower: 25 }
 
+  }
+  var upPower = 1;
+  var player = "";
+  var opponent = "";
+  $('#characterbox').on('click', '.character', function () {
+    $(this).removeClass('character');
+    $(this).addClass('selected');
+    player = $(this).attr('id');
+    console.log("I am player 1" + player);
+    $('#enemy').append($('#characterbox >.character'));
+  });
+
+  $('#enemy').on('click', '.character', function () {
+    $(this).removeClass('character');
+    $(this).addClass('selected');
+    opponent = $(this).attr('id');
+    console.log("I am player 2" + opponent);
+    $('#defender').append($('#enemy >.selected'));
+  });
+  $("#restart").on('click', function () {
+    // location.reload();
+    alert("test");
+
+  });
+
+  $("#attack").click(function () {
+
+    if (objName[opponent]["healthPoints"] > 0 && objName[player]["healthPoints"] > 0) {
+
+      //player attacking opponent
+      var opponentDamage = objName[player]["attackPower"] * (upPower++);
+      $("#line1").html("you attacked " + opponent + " for " + opponentDamage + " damage.");
+      //reducing opponents healthpoints
+      objName[opponent]["healthPoints"] = objName[opponent]["healthPoints"] - opponentDamage;
+      if (objName[opponent]["healthPoints"] < 0) {
+        console.log("test"+ !$.trim( $('#enemy').html() ).length );
+        if(!$.trim( $('#enemy').html() ).length ) {
+          $("#"+opponent).remove();
+          $("#line1").html("You win!!!");
+          $("#line2").html("");
+          $("#line2").append("<button id='restart' type='submit' value='restart'>Restart</button>");
+        }
+        else{
+          $("#"+opponent).remove();
+          $("#line1").html("You have defeated the "+opponent+" you can choose to fight another enemy.");
+          $("#line2").html("");
+        }       
+      }
+      else{
+      $("#" + opponent + " .hPoint").html(objName[opponent]["healthPoints"]);
+      //opponent attacks player now
+      //line printed in user screen
+      $("#line2").html(opponent + " attacked  you back for " + objName[opponent]["counterAttackPower"] + " damage.");
+      //reducing player healthpoints and storing it in object
+      objName[player]["healthPoints"] = objName[player]["healthPoints"] - objName[opponent]["counterAttackPower"];
+      $("#" + player + " .hPoint").html(objName[player]["healthPoints"]);
+
+      if (objName[player]["healthPoints"] < 0) {
+        //updating in User 
+        $("#line1").html("You have been defeated GAME OVER!!!");
+        $("#line2").html("");
+        $("#line2").append("<button id='restart' type='submit' value='restart'>Restart</button>");
+      }
+    }
+      
+    }
+  });
+
+
+
+});
