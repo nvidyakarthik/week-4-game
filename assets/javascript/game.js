58e6c074261b8b700d95aad01aaea482a2c0986f
nvidya.karthik <nvidya.karthik@gmail.com>
2018-02-23 18:42:25 -0700

    applied css to game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,40 +9,56 @@ $(document).ready(function () {
   var upPower = 1;
   var player = "";
   var opponent = "";
+
+  //This function moves the characters down to enemy area
   $('#characterbox').on('click', '.character', function () {
     $(this).removeClass('character');
     $(this).addClass('selected');
     player = $(this).attr('id');
     console.log("I am player 1" + player);
-    $(this).css('background-color', 'green');
-    $('.character').css('background-color', 'red');
+    $(this).css('background-color', '#e8fce8');
+    $('.character').css('background-color', '#ffe3e1');
     $('#enemy').append($('#characterbox >.character'));
   });
 
-  $('#enemy').on('click', '.character', function () {
 
-    $(this).removeClass('character');
-    $(this).addClass('selected');
-    opponent = $(this).attr('id');
-    console.log("I am player 2" + opponent);
-    $("#line1").html("");
-    $("#line2").html("");
-    $('#'+opponent).css('background-color', 'green');       
-    $('#defender').append($('#enemy >.selected'));
+  //This function moves a character to the defender region 
+  $('#enemy').on('click', '.character', function () {
+    if(!$.trim($('#defender').html()).length){
+      $(this).removeClass('character');
+      $(this).addClass('selected');
+      opponent = $(this).attr('id');
+      console.log("I am player 2" + opponent);
+      $("#line1").html("");
+      $("#line2").html("");
+      $('#' + opponent).css('background-color', '#e8fce8');
+      $('#defender').append($('#enemy >.selected'));
+    }
   });
+
+  
+$(".glyphicon").on("click",function() {
+  alert("test");
+  
+});
+
+  //The restart button is dynamically added so adding a onclick on restart button won't work
+  //Do the onclick function on the div that encloses restart button
   $("#line2").on('click', '#restart', function () {
     location.reload();
-    //console.log("inside")
-    //alert("test");
-
   });
 
+  //This function has all the logic when the user clicks the attack button
   $("#attack").click(function () {
+
+    //This condition checks if there is an enemy in the defender area if not prints a message
+    //always use trim there is spaces around the div where the condition won't work
     if (!$.trim($('#defender').html()).length) {
       $("#line1").html("No enemy here pick a new one.")
 
     }
     else {
+      //This condition checks if the healthpoints of both the player is not negative
       if (objName[opponent]["healthPoints"] >= 0 && objName[player]["healthPoints"] >= 0) {
 
         //player attacking opponent
@@ -50,17 +66,18 @@ $(document).ready(function () {
         $("#line1").html("you attacked " + opponent + " for " + opponentDamage + " damage.");
         //reducing opponents healthpoints
         objName[opponent]["healthPoints"] = objName[opponent]["healthPoints"] - opponentDamage;
+        //checking if the opponent has negative health points if yes prints a message saying player wins
         if (objName[opponent]["healthPoints"] < 0) {
           console.log("test" + !$.trim($('#enemy').html()).length);
           if (!$.trim($('#enemy').html()).length) {
             $("#" + opponent).remove();
             $("#line1").html("You win!!!");
             $("#line2").html("");
-            $("#line2").append("<button id='restart' type='submit' value='restart'>Restart</button>");
+            $("#line2").append("<button id='restart' type='submit' class='btn btn-primary'>Restart</button>");
           }
           else {
             $("#" + opponent).remove();
-            $("#line1").html("You have defeated the " + opponent + " you can choose to fight another enemy.");
+            $("#line1").html("You have defeated " + opponent + " you can choose to fight another enemy.");
             $("#line2").html("");
           }
         }
@@ -77,14 +94,11 @@ $(document).ready(function () {
             //updating in User 
             $("#line1").html("You have been defeated GAME OVER!!!");
             $("#line2").html("");
-            $("#line2").append("<button id='restart' type='submit' value='restart'>Restart</button>");
+            $("#line2").append("<button id='restart' type='submit' class='btn btn-primary'>Restart</button>");
           }
         }
 
       }
     }
   });
-
-
-
 });
